d6ffea105cd6def4dec170aeca1476840ea10ab6
irathrone <2668039377@qq.com>
2020-09-20 22:32:15 +0800

    random修改

--- random.py
+++ random.py
@@ -1,6 +1,9 @@
-import random
-x=input()
-xlist=x.split(",")
-xlist=[int(xlist[i])for i in range(len(xlist))]
-some = random.sample(xlist , 1)
-print(some)
+def ran ():
+    import random
+    x=input()
+    xlist=x.split(",")
+    xlist=[int(xlist[i])for i in range(len(xlist))]
+    some = random.sample(xlist , 1)
+    print(some[-1])
+if __name__=='__main__':
+    y=ran()
